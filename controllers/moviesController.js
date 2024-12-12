7cc00d436dca07226821adaaf603cb8458ae2f75
feffe1702 <fpm.asd@gmail.com>
2024-12-12 14:44:40 +0100

    [WIP] create and connect show route

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -10,4 +10,30 @@ function index(req, res) {
         })
     })
 }
-module.exports = { index }
+
+function show(req, res) {
+    const id = req.params.id
+    const movie = `SELECT * FROM movies WHERE id = ?`
+    const review = `SELECT * FROM reviews WHERE movie_id = ?`
+
+    connection.query(movie, [id], (err, results) => {
+        if (err) return res.status(500).json({ err: err })
+        if (results.length == 0) return res.status(404).json({ err: 'Movies not found' })
+        connection.query(review, [id], (err, reviewsResults) => {
+            if (err) return res.status(500).json({ err: err })
+            const movieReview = {
+                ...results[0],
+                reviews: reviewsResults
+            }
+
+            res.json(movieReview)
+        })
+    })
+}
+
+
+
+module.exports = {
+    index,
+    show
+}
--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -4,5 +4,7 @@ const moviesController = require('../controllers/moviesController')
 
 router.get('/', moviesController.index)
 
+router.get('/:id', moviesController.show)
+
 
 module.exports = router
