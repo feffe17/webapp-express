647eb5bb8e6b91bcc1c72dc626a5fe1061b52aae
feffe1702 <fpm.asd@gmail.com>
2024-12-12 13:54:26 +0100

    [WIP] connect db

---
+++ db/posts.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql2')
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: "3306",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+})
+
+connection.connect((err) => {
+    if (err) {
+        console.log('Error', err);
+        return
+    }
+
+    console.log('Database connected');
+})
+
+module.exports = connection
